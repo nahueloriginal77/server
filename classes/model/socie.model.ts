535ecb733e6446545ff0750a202083b775a8af63
unknown <usuario@PUERTA18.CORP.AR>
2022-06-29 14:03:42 -0300

    tvj2

--- classes/model/socie.model.ts
+++ classes/model/socie.model.ts
@@ -1,10 +1,15 @@
 import { Router, Request, Response } from "express";
 import { socie } from "../socie";
-
+export interface socio{
+    nombree:string;
+    apellido:string;
+    nick:string;
+    edad:number;
+}
 export const router = Router();
 
 router.get(`/socies`,(req: Request, res: Response )=> {
-let ficha:socie={nombre: "" ,apellido:"", nick: "", edad:0 };
+let ficha:socie={Nombre: "" ,Apellido:"", nick:"", edad:0 };
 
 socies.array.forEach(socie => {
     if (socie.nick === req.params.nick){
